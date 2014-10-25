43c15799c575cd7ee2b97d8ba70abc50de9b27a2
JomDarbert <ta.herbert@gmail.com>
2014-10-24 17:26:06 -0700

    More testing

--- js/testing.coffee
+++ js/testing.coffee
@@ -1,16 +1,24 @@
 client_id       =   "2721807f620a4047d473472d46865f14"
 SC.initialize client_id: client_id
 
-today            = moment().format("YYYY-MM-DD HH:MM:SS")
-query_start_date = moment().subtract(12, 'months').format("YYYY-MM-DD HH:MM:SS")
+whitelist = [ 
+  "kpop", "k pop", "k-pop", "korean", "korea", "kor", "kor pop", 
+  "korean pop", "korean-pop", "kor-pop", "korean version", "kr",
+  "kr ver", "original"
+  ]
 
-whitelist       = [ "kpop", "k pop", "k-pop", "korean", "korea"]
-blacklist       = [ "cover", "acoustic", "instrumental", "remix", "mix", "re mix", 
-                    "re-mix", "version", "ver.", "cvr"]
+blacklist = [ 
+  "cover", "acoustic", "instrumental", "remix", "mix", "re mix", "re-mix", 
+  "version", "ver.", "live", "live cover", "accapella", "cvr", "united states", 
+  "america", "india", "indian", "japan", "china", "chinese", "japanese", "viet", 
+  "vietnam", "vietnamese", "thai", "taiwan", "taiwanese", "russian", "ambient",
+  "meditat"
+  ]
 
-hangul_chars    =   "[\u1100-\u11FF|\u3130-\u318F|\uA960-\uA97F|\uAC00-\uD7AF|"+
-                    "\uD7B0-\uD7FF]"
+hangul_chars    =   "[^\u1100-\u11FF|^\u3130-\u318F|^\uA960-\uA97F|^\uAC00-\uD7AF|^\uD7B0-\uD7FF]"
 
+english_chars   = "[]"
+english         = new RegExp english_chars
 hangul          = new RegExp hangul_chars
 
 eyk = (->
@@ -42,6 +50,7 @@ mwave = (->
 )()
 
 top_queries = []
+console.log hangul.test("안녕 name 안녕[].")
 
 for song in eyk
     top_queries.push song.title
@@ -50,26 +59,81 @@ for song in mwave
     top_queries.push song.artist + " " + song.title
 
 
-(->
-  processTracks = (tracks) ->
-    trackArray = []
-    tracks.forEach (track) ->
+checkBlacklist = (song) ->
+  # Don't want songs where critical values are missing
+  if not song.title?    then return false
+  if not song.url?      then return false
+  if not song.created?  then return false
 
-      if track.title?       then title    = track.title.toLowerCase()
-      if track.genre?       then genre    = track.genre.toLowerCase()
-      if track.tag_list?    then tags     = track.tag_list.toLowerCase().split(" ")
-      if track.created_at?  then created  = moment(track.created_at)
+  # Don't want songs that were created more than one year ago
+  ok_months    = 12
+  created_date = moment(song.created, "YYYY-MM-DD HH:MM:SS")
+  date_limit   = moment().subtract(ok_months, "months")
 
-      if genre in whitelist
-        console.log title,genre
+  if date_limit.diff(created_date, "months") > 0 then return false
+
+  # Don't want songs with blacklist words in title
+  for term in blacklist
+    if song.title.indexOf(term) isnt -1 then return false
+
+  # Don't want songs with blacklist words in genre
+  for term in blacklist
+    if song.genre? and song.genre.indexOf(term) isnt -1 then return false
+
+  # Don't want songs with blacklist words in tags
+  for term in blacklist
+    if term in song.tags then return false
+
+  # Don't want songs with characters that aren't English or Korean
 
 
-      trackArray.push song
-      return
+  # Song passed all checks
+  return true
 
-    #console.log trackArray
+
+checkWhitelist = (song) ->
+  points = 0
+  if song.genre in whitelist then points += 1
+
+
+processTracks = (tracks) ->
+  songArray = []
+  tracks.forEach (track) ->
+    if track.title?       then title    = track.title.toLowerCase()
+    if track.genre?       then genre    = track.genre.toLowerCase()
+    if track.tag_list?    then tags     = track.tag_list.toLowerCase().split(" ")
+    if track.created_at?  then created  = track.created_at
+    if track.stream_url?  then url      = track.stream_url
+    if track.artwork_url? then artwork  = track.artwork_url.replace("-large","-t500x500")
+
+    songObj =
+      title: title
+      genre: genre
+      tags: tags
+      created: created
+      url: url
+      artwork: artwork
+
+    blacklist_pass = checkBlacklist(songObj)
+    #whitelist_score = checkWhitelist(songObj)
+
+
+    ###
+
+    for tag in tags when tag in whitelist
+      points += 1
+    ###
+
+
+
     return
 
+  for song in songArray
+    console.log song
+  return
+
+
+(->
   SC.initialize client_id: client_id
 
   for query in top_queries
--- js/testing.js
+++ js/testing.js
@@ -1,5 +1,5 @@
 (function() {
-  var blacklist, client_id, eyk, hangul, hangul_chars, mwave, query_start_date, song, today, top_queries, whitelist, _i, _j, _len, _len1,
+  var blacklist, checkBlacklist, checkWhitelist, client_id, english, english_chars, eyk, hangul, hangul_chars, mwave, processTracks, song, top_queries, whitelist, _i, _j, _len, _len1,
     __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   client_id = "2721807f620a4047d473472d46865f14";
@@ -8,15 +8,15 @@
     client_id: client_id
   });
 
-  today = moment().format("YYYY-MM-DD HH:MM:SS");
+  whitelist = ["kpop", "k pop", "k-pop", "korean", "korea", "kor", "kor pop", "korean pop", "korean-pop", "kor-pop", "korean version", "kr", "kr ver", "original"];
 
-  query_start_date = moment().subtract(12, 'months').format("YYYY-MM-DD HH:MM:SS");
+  blacklist = ["cover", "acoustic", "instrumental", "remix", "mix", "re mix", "re-mix", "version", "ver.", "live", "live cover", "accapella", "cvr", "united states", "america", "india", "indian", "japan", "china", "chinese", "japanese", "viet", "vietnam", "vietnamese", "thai", "taiwan", "taiwanese", "russian", "ambient", "meditat"];
 
-  whitelist = ["kpop", "k pop", "k-pop", "korean", "korea"];
+  hangul_chars = "[^\u1100-\u11FF|^\u3130-\u318F|^\uA960-\uA97F|^\uAC00-\uD7AF|^\uD7B0-\uD7FF]";
 
-  blacklist = ["cover", "acoustic", "instrumental", "remix", "mix", "re mix", "re-mix", "version", "ver.", "cvr"];
+  english_chars = "[]";
 
-  hangul_chars = "[\u1100-\u11FF|\u3130-\u318F|\uA960-\uA97F|\uAC00-\uD7AF|" + "\uD7B0-\uD7FF]";
+  english = new RegExp(english_chars);
 
   hangul = new RegExp(hangul_chars);
 
@@ -52,6 +52,8 @@
 
   top_queries = [];
 
+  console.log(hangul.test("안녕 name 안녕[]."));
+
   for (_i = 0, _len = eyk.length; _i < _len; _i++) {
     song = eyk[_i];
     top_queries.push(song.title);
@@ -62,31 +64,99 @@
     top_queries.push(song.artist + " " + song.title);
   }
 
+  checkBlacklist = function(song) {
+    var created_date, date_limit, ok_months, term, _k, _l, _len2, _len3, _len4, _m;
+    if (song.title == null) {
+      return false;
+    }
+    if (song.url == null) {
+      return false;
+    }
+    if (song.created == null) {
+      return false;
+    }
+    ok_months = 12;
+    created_date = moment(song.created, "YYYY-MM-DD HH:MM:SS");
+    date_limit = moment().subtract(ok_months, "months");
+    if (date_limit.diff(created_date, "months") > 0) {
+      return false;
+    }
+    for (_k = 0, _len2 = blacklist.length; _k < _len2; _k++) {
+      term = blacklist[_k];
+      if (song.title.indexOf(term) !== -1) {
+        return false;
+      }
+    }
+    for (_l = 0, _len3 = blacklist.length; _l < _len3; _l++) {
+      term = blacklist[_l];
+      if ((song.genre != null) && song.genre.indexOf(term) !== -1) {
+        return false;
+      }
+    }
+    for (_m = 0, _len4 = blacklist.length; _m < _len4; _m++) {
+      term = blacklist[_m];
+      if (__indexOf.call(song.tags, term) >= 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  checkWhitelist = function(song) {
+    var points, _ref;
+    points = 0;
+    if (_ref = song.genre, __indexOf.call(whitelist, _ref) >= 0) {
+      return points += 1;
+    }
+  };
+
+  processTracks = function(tracks) {
+    var songArray, _k, _len2;
+    songArray = [];
+    tracks.forEach(function(track) {
+      var artwork, blacklist_pass, created, genre, songObj, tags, title, url;
+      if (track.title != null) {
+        title = track.title.toLowerCase();
+      }
+      if (track.genre != null) {
+        genre = track.genre.toLowerCase();
+      }
+      if (track.tag_list != null) {
+        tags = track.tag_list.toLowerCase().split(" ");
+      }
+      if (track.created_at != null) {
+        created = track.created_at;
+      }
+      if (track.stream_url != null) {
+        url = track.stream_url;
+      }
+      if (track.artwork_url != null) {
+        artwork = track.artwork_url.replace("-large", "-t500x500");
+      }
+      songObj = {
+        title: title,
+        genre: genre,
+        tags: tags,
+        created: created,
+        url: url,
+        artwork: artwork
+      };
+      blacklist_pass = checkBlacklist(songObj);
+
+      /*
+      
+      for tag in tags when tag in whitelist
+        points += 1
+       */
+    });
+    for (_k = 0, _len2 = songArray.length; _k < _len2; _k++) {
+      song = songArray[_k];
+      console.log(song);
+    }
+  };
+
   (function() {
-    var processTracks, query, _k, _len2, _results;
-    processTracks = function(tracks) {
-      var trackArray;
-      trackArray = [];
-      tracks.forEach(function(track) {
-        var created, genre, tags, title;
-        if (track.title != null) {
-          title = track.title.toLowerCase();
-        }
-        if (track.genre != null) {
-          genre = track.genre.toLowerCase();
-        }
-        if (track.tag_list != null) {
-          tags = track.tag_list.toLowerCase().split(" ");
-        }
-        if (track.created_at != null) {
-          created = moment(track.created_at);
-        }
-        if (__indexOf.call(whitelist, genre) >= 0) {
-          console.log(title, genre);
-        }
-        trackArray.push(song);
-      });
-    };
+    var query, _k, _len2, _results;
     SC.initialize({
       client_id: client_id
     });
